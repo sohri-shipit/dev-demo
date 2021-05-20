79ddaada984be569549e5b8f47a144587a41ec7f
test <test@tests-MacBook-Pro.local>
2021-05-19 19:42:37 -0700

    Revert "added old python practise files"
    This reverts commit 1aa67859fc7e35f449e8da1ad4c0f464f3449ddb.

--- numList2.py
+++
@@ -1,3 +0,0 @@
-odd_list = list(range(1,21,2))
-for num in odd_list:
-	print(f"the odd number is {num}")
--- numList3_0508.py
+++
@@ -1,8 +0,0 @@
-numb = list(range(1,11))
-cube_list = []
-for num in numb:
-	cube_list.append(num ** 3)
-
-print(f"the cube list is {cube_list}")
-comp_cube = [num1 ** 3 for num1 in numb]
-print(f"the cube in comprehension is {comp_cube}")
