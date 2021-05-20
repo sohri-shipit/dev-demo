dc3bce7b3f76887eb917d01ca864871e885e9dc6
test <test@tests-MacBook-Pro.local>
2021-05-19 19:38:54 -0700

    Revert "added old python practise files"
    This reverts commit 1aa67859fc7e35f449e8da1ad4c0f464f3449ddb.

--- numberList1_0508.py
+++
@@ -1,9 +0,0 @@
-#Summing a million
-mill_list = list(range(1,1000001))
-#print(mill_list)
-minimum = min(mill_list)
-maximum = max(mill_list)
-addition = sum(mill_list)
-print(f"The min number is: {minimum}")
-print(f"Tne max number in list is: {maximum}")
-print(f"The sum is {addition}")
